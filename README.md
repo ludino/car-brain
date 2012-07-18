caa8548380f210e72ccf43505ebc639fc3e71f56
Stian Fauskanger <stianfauskanger@gmail.com>
2012-07-18 21:34:22 +0200

    Added the first source files

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ car-brain
 =========
 
 We will try to build a genetic algorithm to generate a "brain" which will steer a car
+
+Ludi.no
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    std::cout << "Hello brain\n";
+    return 0;
+}
+
