c9f60200faec291cbe3dbf84d049d811b9c64045
fauskanger <thomasfauskanger@gmail.com>
2012-08-08 23:45:43 +0200

    git test Thomas

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, const char * argv[]) {
     Brain myBrain;
     std::cout << "Hello brain\n";
 
-	std::cout << "\nPress Enter to exit. . .\n";
+	std::cout << "\nPress Enter to exit. . .\n"; // Foo bar test
 	std::cin.get();
     return 0;
 }
